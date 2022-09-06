555d2227935db2bf4e72a967a292ec9e8635bc15
Alexander Timonov <astimonov@gmail.com>
2022-09-06 22:13:32 +0300

    Add registers to CPU

--- src/emulator/cpu.hpp
+++ src/emulator/cpu.hpp
@@ -3,14 +3,36 @@
 
 #include "memory_controller.hpp"
 #include "memory_interface.hpp"
+#include "register.hpp"
 #include "types.hpp"
 
 namespace Goodboy
 {
     class Cpu
     {
+    public:
+        Cpu()
+            : AF(A, F),
+              BC(B, C),
+              DE(D, E),
+              HL(H, L),
+              Flags(AF)
+        {
+
+        }
+
     private:
         MemoryController mMemoryController = MemoryController();
+        Register<Byte> A, F,
+                       B, C,
+                       D, E,
+                       H, L;
+        RegisterPair<> AF,
+                       BC,
+                       DE,
+                       HL;
+        FlagRegister   Flags;
+        Register<Word> SP, PC;
     };
 }
 
