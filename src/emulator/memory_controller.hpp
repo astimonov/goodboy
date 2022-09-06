60d1c1b6a492e7e0b7cc5a26361c64cd75cffbc4
Alexander Timonov <astimonov@gmail.com>
2022-09-06 22:13:18 +0300

    Refactor memory controller

--- src/emulator/memory_controller.hpp
+++ src/emulator/memory_controller.hpp
@@ -14,19 +14,22 @@ namespace Goodboy
     {
         None = 0,
         Read = 1,
-        Write = 2
+        Write = 2,
+        ReadWrite = AccessMode::Read | AccessMode::Write,
     };
 
     constexpr AccessMode operator|(AccessMode a, AccessMode b)
     {
-        return static_cast<AccessMode>(static_cast<std::underlying_type_t<AccessMode>>(a) |
-                                       static_cast<std::underlying_type_t<AccessMode>>(b));
+        using AccessModeType = std::underlying_type_t<AccessMode>;
+        return static_cast<AccessMode>(static_cast<AccessModeType>(a) |
+                                       static_cast<AccessModeType>(b));
     }
 
     constexpr AccessMode operator&(AccessMode a, AccessMode b)
     {
-        return static_cast<AccessMode>(static_cast<std::underlying_type_t<AccessMode>>(a) &
-                                       static_cast<std::underlying_type_t<AccessMode>>(b));
+        using AccessModeType = std::underlying_type_t<AccessMode>;
+        return static_cast<AccessMode>(static_cast<AccessModeType>(a) &
+                                       static_cast<AccessModeType>(b));
     }
 
     struct Mapping
@@ -54,64 +57,64 @@ namespace Goodboy
             return static_cast<std::size_t>(end) - static_cast<std::size_t>(start);
         }
 
-        static constexpr Address RomBank00Start     = 0x0000;
-        static constexpr Address RomBank01Start     = 0x4000;
-        static constexpr Address VideoRamStart      = 0x8000;
-        static constexpr Address ExternalRamStart   = 0xA000;
-        static constexpr Address Wram0Start         = 0xC000;
-        static constexpr Address Wram1Start         = 0xD000;
-        static constexpr Address EchoRam0Start      = 0xE000;
-        static constexpr Address EchoRam1Start      = 0xF000;
-        static constexpr Address OamStart           = 0xFE00;
-        static constexpr Address NotUsableStart     = 0xFEA0;
-        static constexpr Address IoStart            = 0xFF00;
-        static constexpr Address HramStart          = 0xFF80;
-        static constexpr Address IeStart            = 0xFFFF;
+        static constexpr Address RomBank00Start     { 0x0000 };
+        static constexpr Address RomBank01Start     { 0x4000 };
+        static constexpr Address VideoRamStart      { 0x8000 };
+        static constexpr Address ExternalRamStart   { 0xA000 };
+        static constexpr Address Wram0Start         { 0xC000 };
+        static constexpr Address Wram1Start         { 0xD000 };
+        static constexpr Address EchoRam0Start      { 0xE000 };
+        static constexpr Address EchoRam1Start      { 0xF000 };
+        static constexpr Address OamStart           { 0xFE00 };
+        static constexpr Address NotUsableStart     { 0xFEA0 };
+        static constexpr Address IoStart            { 0xFF00 };
+        static constexpr Address HramStart          { 0xFF80 };
+        static constexpr Address IeStart            { 0xFFFF };
 
-        static constexpr Address RomBank00End       = 0x3FFF;
-        static constexpr Address RomBank01End       = 0x7FFF;
-        static constexpr Address VideoRamEnd        = 0x9FFF;
-        static constexpr Address ExternalRamEnd     = 0xBFFF;
-        static constexpr Address Wram0End           = 0xCFFF;
-        static constexpr Address Wram1End           = 0xDFFF;
-        static constexpr Address EchoRam0End        = 0xEFFF;
-        static constexpr Address EchoRam1End        = 0xFDFF;
-        static constexpr Address OamEnd             = 0xFE9F;
-        static constexpr Address NotUsableEnd       = 0xFEFF;
-        static constexpr Address IoEnd              = 0xFF7F;
-        static constexpr Address HramEnd            = 0xFFFE;
-        static constexpr Address IeEnd              = 0xFFFF;
+        static constexpr Address RomBank00End       { 0x3FFF };
+        static constexpr Address RomBank01End       { 0x7FFF };
+        static constexpr Address VideoRamEnd        { 0x9FFF };
+        static constexpr Address ExternalRamEnd     { 0xBFFF };
+        static constexpr Address Wram0End           { 0xCFFF };
+        static constexpr Address Wram1End           { 0xDFFF };
+        static constexpr Address EchoRam0End        { 0xEFFF };
+        static constexpr Address EchoRam1End        { 0xFDFF };
+        static constexpr Address OamEnd             { 0xFE9F };
+        static constexpr Address NotUsableEnd       { 0xFEFF };
+        static constexpr Address IoEnd              { 0xFF7F };
+        static constexpr Address HramEnd            { 0xFFFE };
+        static constexpr Address IeEnd              { 0xFFFF };
 
-        RandomAccessMemory mRomBank00   = RandomAccessMemory(GetMappingSize(RomBank00Start,     RomBank00End));
-        RandomAccessMemory mRomBank01   = RandomAccessMemory(GetMappingSize(RomBank01Start,     RomBank01End));
-        RandomAccessMemory mVideoRam    = RandomAccessMemory(GetMappingSize(VideoRamStart,      VideoRamEnd));
-        RandomAccessMemory mExternalRam = RandomAccessMemory(GetMappingSize(ExternalRamStart,   ExternalRamEnd));
-        RandomAccessMemory mWram0       = RandomAccessMemory(GetMappingSize(Wram0Start,         Wram0End));
-        RandomAccessMemory mWram1       = RandomAccessMemory(GetMappingSize(Wram1Start,         Wram1End));
-        RandomAccessMemory mOam         = RandomAccessMemory(GetMappingSize(OamStart,           OamEnd));
-        RandomAccessMemory mIo          = RandomAccessMemory(GetMappingSize(IoStart,            IoEnd));
-        RandomAccessMemory mHram        = RandomAccessMemory(GetMappingSize(HramStart,          HramEnd));
-        RandomAccessMemory mIe          = RandomAccessMemory(GetMappingSize(IeStart,            IeEnd));
-        StubMemory         mStubMemory  = StubMemory();
+        RandomAccessMemory mRomBank00   { GetMappingSize(RomBank00Start,     RomBank00End) };
+        RandomAccessMemory mRomBank01   { GetMappingSize(RomBank01Start,     RomBank01End) };
+        RandomAccessMemory mVideoRam    { GetMappingSize(VideoRamStart,      VideoRamEnd) };
+        RandomAccessMemory mExternalRam { GetMappingSize(ExternalRamStart,   ExternalRamEnd) };
+        RandomAccessMemory mWram0       { GetMappingSize(Wram0Start,         Wram0End) };
+        RandomAccessMemory mWram1       { GetMappingSize(Wram1Start,         Wram1End) };
+        RandomAccessMemory mOam         { GetMappingSize(OamStart,           OamEnd) };
+        RandomAccessMemory mIo          { GetMappingSize(IoStart,            IoEnd) };
+        RandomAccessMemory mHram        { GetMappingSize(HramStart,          HramEnd) };
+        RandomAccessMemory mIe          { GetMappingSize(IeStart,            IeEnd) };
+        StubMemory         mStubMemory;
 
-        std::vector<Mapping> mMapping =
+        std::vector<Mapping> mMapping
         {
-            {RomBank00Start,    RomBank00End,   AccessMode::Read,                       mRomBank00},
-            {RomBank01Start,    RomBank01End,   AccessMode::Read,                       mRomBank00},
-            {VideoRamStart,     VideoRamEnd,    AccessMode::Read | AccessMode::Write,   mVideoRam},
-            {ExternalRamStart,  ExternalRamEnd, AccessMode::Read | AccessMode::Write,   mExternalRam},
-            {Wram0Start,        Wram0End,       AccessMode::Read | AccessMode::Write,   mWram0},
-            {Wram1Start,        Wram1End,       AccessMode::Read | AccessMode::Write,   mWram1},
-            {EchoRam0Start,     EchoRam0End,    AccessMode::Read,                       mWram0},
-            {EchoRam1Start,     EchoRam1End,    AccessMode::Read,                       mWram1},
-            {OamStart,          OamEnd,         AccessMode::Read | AccessMode::Write,   mOam},
-            {NotUsableStart,    NotUsableEnd,   AccessMode::None,                       mStubMemory},
-            {IoStart,           IoEnd,          AccessMode::Read | AccessMode::Write,   mIo},
-            {HramStart,         HramEnd,        AccessMode::Read | AccessMode::Write,   mHram},
-            {IeStart,           IeEnd,          AccessMode::Read | AccessMode::Write,   mIe}
+            {RomBank00Start,    RomBank00End,   AccessMode::Read,       mRomBank00},
+            {RomBank01Start,    RomBank01End,   AccessMode::Read,       mRomBank01},
+            {VideoRamStart,     VideoRamEnd,    AccessMode::ReadWrite,  mVideoRam},
+            {ExternalRamStart,  ExternalRamEnd, AccessMode::ReadWrite,  mExternalRam},
+            {Wram0Start,        Wram0End,       AccessMode::ReadWrite,  mWram0},
+            {Wram1Start,        Wram1End,       AccessMode::ReadWrite,  mWram1},
+            {EchoRam0Start,     EchoRam0End,    AccessMode::Read,       mWram0},
+            {EchoRam1Start,     EchoRam1End,    AccessMode::Read,       mWram1},
+            {OamStart,          OamEnd,         AccessMode::ReadWrite,  mOam},
+            {NotUsableStart,    NotUsableEnd,   AccessMode::None,       mStubMemory},
+            {IoStart,           IoEnd,          AccessMode::ReadWrite,  mIo},
+            {HramStart,         HramEnd,        AccessMode::ReadWrite,  mHram},
+            {IeStart,           IeEnd,          AccessMode::ReadWrite,  mIe}
         };
 
-        Mapping mFaultMapping = {0, 0, AccessMode::None, mStubMemory};
+        Mapping mFaultMapping { 0, 0, AccessMode::None, mStubMemory };
 
         Mapping& GetMapping(Address address);
         Mapping& GetMapping(Address address, AccessMode accessMode);
